4ff5b89baed6048f238bdd7aaa580969b0b7b8e9
Steven Yan <examsherpa@gmail.com>
2021-03-11 15:37:19 -0500

    update files

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 import seaborn as sns
-from sklearn.metrics import precision_score, recall_score, f1_score, average_precision_score, confusion_matrix, roc_auc_score, plot_precision_recall_curve
+from sklearn.metrics import precision_score, recall_score, f1_score, average_precision_score, confusion_matrix, auc
 from tqdm import tqdm
 from nltk.stem.porter import PorterStemmer
 from textblob import Word
@@ -9,7 +9,9 @@ import re
 import nltk
 import pandas as pd
 import requests
+#!/usr/bin/env python
 import config
+import matplotlib.pyplot as plt
 
 def group_list(lst, size=100):
     """
@@ -35,7 +37,7 @@ def tweets_request(tweets_ids):
     for batch in tqdm(tweets_ids):
         url = "https://api.twitter.com/2/tweets?ids={}&tweet.fields=created_at&expansions=author_id&user.fields=created_at".format(batch)
         payload={}
-        headers = {'Authorization': 'Bearer ' + config.bearer_token,
+        headers = {'Authorization': 'Bearer ' + config.keys['bearer_token'],
         'Cookie': 'personalization_id="v1_hzpv7qXpjB6CteyAHDWYQQ=="; guest_id=v1%3A161498381400435837'}
         r = requests.request("GET", url, headers=headers, data=payload)
         data = r.json()
@@ -44,20 +46,39 @@ def tweets_request(tweets_ids):
     
     return pd.concat(df_lst)
 
-
-def get_metrics(X, y, y_hat, model):
-    print("Training Precision: ", precision_score(y_tr, y_hat_tr))
-    print("Testing Precision: ", precision_score(y_tt, y_hat_tt))
-    print("Training Recall: ", recall_score(y_tr, y_hat_tr))
-    print()
-
 def aps(X, y, model):
+    """
+        Function to calculate PR AUC Score based on predict_proba(X)
+        where X is feature values, y is target values, and model is instantiated model variable
+    """
     probs = model.predict_proba(X)[:,1]
     return average_precision_score(y, probs)
 
-def aps2(X, y, model):
-    probs = model.decision_function(X)
-    return average_precision_score(y, probs)
+def get_metrics(X_tr, y_tr, X_val, y_val, y_pred_tr, y_pred_val, model):
+    """
+        Function to get training and validation F1, recall, precision, PR AUC scores
+        Instantiate model and pass the model into function
+        Pass X_train, y_train, X_val, Y_val datasets
+        Pass in calculated model.predict(X) for y_pred
+    """    
+    f1_tr = f1_score(y_tr, y_pred_tr)
+    f1_val = f1_score(y_val, y_pred_val)
+    rc_tr = recall_score(y_tr, y_pred_tr)
+    rc_val = recall_score(y_val, y_pred_val)
+    pr_tr = precision_score(y_tr, y_pred_tr)
+    pr_val = precision_score(y_val, y_pred_val)
+    aps_tr = aps(X_tr, y_tr, model)
+    aps_val = aps(X_val, y_val, model)
+    
+    print('Training F1 Score: ', f1_tr)
+    print('Validation F1 Score: ', f1_val)
+    print('Training Recall Score: ', rc_tr)
+    print('Validation Recall Score: ', rc_val)
+    print('Training Precision Score: ', pr_tr)
+    print('Validation Precision Score: ', pr_val)
+    print('Training Average Precision Score: ', aps_tr)
+    print('Validation Average Precision Score: ', aps_val)
+
 
     
 def conf_matrix(y_test, y_hat_test):
@@ -68,6 +89,94 @@ def conf_matrix(y_test, y_hat_test):
     labels = [f'{v1}\n{v2}\n{v3}' for v1, v2, v3 in zip(group_names, group_counts, group_percentages)]
     labels = np.asarray(labels).reshape(2,2)
     sns.heatmap(cnf, annot=labels, fmt='', cmap='Blues', annot_kws={'size':16})
+
+# courtesy of DTrimarchi10 on Github
+def make_confusion_matrix(cf, X, y, model,
+                          group_names=['TN','FP','FN','TP'],
+                          categories='auto',
+                          count=True,
+                          percent=True,
+                          cbar=True,
+                          xyticks=True,
+                          xyplotlabels=True,
+                          sum_stats=True,
+                          figsize=None,
+                          cmap='Blues',
+                          title=None):
+    '''
+    This function will make a pretty plot of an sklearn Confusion Matrix cm using a Seaborn heatmap visualization.
+    Arguments
+    ---------
+    cf:            confusion matrix to be passed in
+    group_names:   List of strings that represent the labels row by row to be shown in each square.
+    categories:    List of strings containing the categories to be displayed on the x,y axis. Default is 'auto'
+    count:         If True, show the raw number in the confusion matrix. Default is True.
+    normalize:     If True, show the proportions for each category. Default is True.
+    cbar:          If True, show the color bar. The cbar values are based off the values in the confusion matrix.
+                   Default is True.
+    xyticks:       If True, show x and y ticks. Default is True.
+    xyplotlabels:  If True, show 'True Label' and 'Predicted Label' on the figure. Default is True.
+    sum_stats:     If True, display summary statistics below the figure. Default is True.
+    figsize:       Tuple representing the figure size. Default will be the matplotlib rcParams value.
+    cmap:          Colormap of the values displayed from matplotlib.pyplot.cm. Default is 'Blues'
+                   See http://matplotlib.org/examples/color/colormaps_reference.html                  
+    title:         Title for the heatmap. Default is None.
+    '''
+
+    # CODE TO GENERATE TEXT INSIDE EACH SQUARE
+    blanks = ['' for i in range(cf.size)]
+    if group_names and len(group_names)==cf.size:
+        group_labels = ["{}\n".format(value) for value in group_names]
+    else:
+        group_labels = blanks
+    if count:
+        group_counts = ["{0:0.0f}\n".format(value) for value in cf.flatten()]
+    else:
+        group_counts = blanks
+    if percent:
+        group_percentages = ["{0:.2%}".format(value) for value in cf.flatten()/np.sum(cf)]
+    else:
+        group_percentages = blanks
+    box_labels = [f"{v1}{v2}{v3}".strip() for v1, v2, v3 in zip(group_labels,group_counts,group_percentages)]
+    box_labels = np.asarray(box_labels).reshape(cf.shape[0],cf.shape[1])
+
+    # CODE TO GENERATE SUMMARY STATISTICS & TEXT FOR SUMMARY STATS
+    if sum_stats:
+        #Accuracy is sum of diagonal divided by total observations
+        accuracy  = np.trace(cf) / float(np.sum(cf))
+        #if it is a binary confusion matrix, show some more stats
+        if len(cf)==2:
+            #Metrics for Binary Confusion Matrices
+            precision = cf[1,1] / sum(cf[:,1])
+            recall    = cf[1,1] / sum(cf[1,:])
+            f1_score  = 2*precision*recall / (precision + recall)
+            pr_auc = aps(X, y, model)
+            stats_text = "\n\nPrecision={:0.3f}\nRecall={:0.3f}\nF1 Score={:0.3f}\nPR AUC Score={:0.3f}".format(
+                precision,recall,f1_score, pr_auc)
+        else:
+            stats_text = "\n\nAccuracy={:0.3f}".format(accuracy)
+    else:
+        stats_text = ""
+
+    # SET FIGURE PARAMETERS ACCORDING TO OTHER ARGUMENTS
+    if figsize==None:
+        #Get default figure size if not set
+        figsize = plt.rcParams.get('figure.figsize')
+    if xyticks==False:
+        #Do not show categories if xyticks is False
+        categories=False
+
+    # MAKE THE HEATMAP VISUALIZATION
+    plt.figure(figsize=figsize)
+    sns.heatmap(cf,annot=box_labels,fmt="",cmap=cmap,cbar=cbar,xticklabels=categories,yticklabels=categories)
+    if xyplotlabels:
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label' + stats_text)
+    else:
+        plt.xlabel(stats_text)    
+    if title:
+        plt.title(title)
+
 
 def num_of_words(df, col):
     df['word_ct'] = df[col].apply(lambda x: len(str(x).split(" ")))
