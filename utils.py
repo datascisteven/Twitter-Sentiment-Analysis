bf421d2f40979778aa226945d082ba347f6de4a2
Steven Yan <examsherpa@gmail.com>
2021-03-11 11:20:58 -0500

    update changes

---
+++ utils.py
@@ -0,0 +1,169 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn.metrics import precision_score, recall_score, accuracy_score, f1_score, auc, average_precision_score, confusion_matrix, roc_auc_score, plot_precision_recall_curve
+from collections import Counter
+from tqdm import tqdm
+from nltk.stem.porter import PorterStemmer
+from textblob import Word
+from nltk.corpus import stopwords
+import re
+import nltk
+import pandas as pd
+import requests
+import config
+
+def group_list(lst, size=100):
+    """
+    Generate batches of 100 ids in each
+    Returns list of strings with , seperated ids
+    """
+    new_list =[]
+    idx = 0
+    while idx < len(lst):        
+        new_list.append(
+            ','.join([str(item) for item in lst[idx:idx+size]])
+        )
+        idx += size
+    return new_list
+
+
+def tweets_request(tweets_ids):
+    """
+    Make a requests to Tweeter API
+    """
+    df_lst = []
+    
+    for batch in tqdm(tweets_ids):
+        url = "https://api.twitter.com/2/tweets?ids={}&tweet.fields=created_at&expansions=author_id&user.fields=created_at".format(batch)
+        payload={}
+        headers = {'Authorization': 'Bearer ' + config.bearer_token,
+        'Cookie': 'personalization_id="v1_hzpv7qXpjB6CteyAHDWYQQ=="; guest_id=v1%3A161498381400435837'}
+        r = requests.request("GET", url, headers=headers, data=payload)
+        data = r.json()
+        if 'data' in data.keys():
+            df_lst.append(pd.DataFrame(data['data']))
+    
+    return pd.concat(df_lst)
+
+
+def accuracy(y, y_hat):
+    
+    y_y_hat = list(zip(y, y_hat))
+    tp = sum([1 for i in y_y_hat if i[0] == 1 and i[1] == 1])
+    tn = sum([1 for i in y_y_hat if i[0] == 0 and i[1] == 0])
+    return (tp + tn) / float(len(y_y_hat))
+
+def f1(y, y_hat):
+    precision_score = precision(y, y_hat)
+    recall_score = recall(y, y_hat)
+    numerator = precision_score * recall_score
+    denominator = precision_score + recall_score
+    return 2 * (numerator / denominator)
+
+def precision(y, y_hat):
+    y_y_hat = list(zip(y, y_hat))
+    tp = sum([1 for i in y_y_hat if i[0] == 1 and i[1] == 1])
+    fp = sum([1 for i in y_y_hat if i[0] == 0 and i[1] == 1])
+    return tp / float(tp + fp)
+
+def recall(y, y_hat):
+    # Your code here
+    y_y_hat = list(zip(y, y_hat))
+    tp = sum([1 for i in y_y_hat if i[0] == 1 and i[1] == 1])
+    fn = sum([1 for i in y_y_hat if i[0] == 1 and i[1] == 0])
+    return tp / float(tp + fn)
+
+def auc(X, y, model):
+    probs = model.predict_proba(X)[:,1] 
+    return roc_auc_score(y, probs)
+
+def auc2(X, y, model):
+    probs = model.decision_function(X)
+    return roc_auc_score(y, probs)
+
+def aps(X, y, model):
+    probs = model.predict_proba(X)[:,1]
+    return average_precision_score(y, probs)
+
+def aps2(X, y, model):
+    probs = model.decision_function(X)
+    return average_precision_score(y, probs)
+
+    
+def conf_matrix(y_test, y_hat_test):
+    cnf = confusion_matrix(y_test, y_hat_test)
+    group_names = ['TN','FP','FN','TP']
+    group_counts = ['{0:0.0f}'.format(value) for value in cnf.flatten()]
+    group_percentages = ['{0:.2%}'.format(value) for value in cnf.flatten()/np.sum(cnf)]
+    labels = [f'{v1}\n{v2}\n{v3}' for v1, v2, v3 in zip(group_names, group_counts, group_percentages)]
+    labels = np.asarray(labels).reshape(2,2)
+    sns.heatmap(cnf, annot=labels, fmt='', cmap='Blues', annot_kws={'size':16})
+
+def num_of_words(df, col):
+    df['word_ct'] = df[col].apply(lambda x: len(str(x).split(" ")))
+    print(df[[col, 'word_ct']])
+
+def num_of_chars(df, col):
+    df['char_ct'] = df[col].str.len()
+    print(df[[col, 'char_ct']])
+
+def avg_word(sentence):
+    words = sentence.split()
+    return (sum(len(word) for word in words)/len(words))
+
+def avg_word_length(df, col):
+    df['avg_wrd'] = df[col].apply(lambda x: avg_word(x))
+    print(df[[col, 'avg_wrd']].head())
+
+def hash_tags(df, col):
+    df['hashtags'] = df[col].apply(lambda x: len(re.split(r'#', str(x)))-1)
+    print(df[[col, 'hashtags']].head())
+
+def preprocess_tweet(df, col):
+    """
+        Remove callouts, character references (HTML characters, emojis), # in hashtags, 
+        Remove Twitter code RT and QT, URL links, punctuation, excess whitespace between
+        Lowercase all words and remove leading and trailing whitespaces
+    """
+    df[col] = df[col].apply(lambda x: re.sub(r'@[\S]+', ' ', str(x)))
+    df[col] = df[col].apply(lambda x: re.sub(r'&[\S]+?;', ' ', str(x)))
+    df[col] = df[col].apply(lambda x: re.sub(r'#', ' ', str(x)))
+    df[col] = df[col].apply(lambda x: re.sub(r'(\bRT\b|\bQT\b)', ' ', str(x)))
+    df[col] = df[col].apply(lambda x: re.sub(r'http[\S]+', ' ', str(x)))
+    df[col] = df[col].apply(lambda x: re.sub(r'[^\w\s]', r'', str(x)))
+    df[col] = df[col].apply(lambda x: " ".join(x.lower() for x in x.split()))
+    df[col] = df[col].apply(lambda x: re.sub(r'\w*\d\w*', r' ', str(x)))
+    df[col] = df[col].apply(lambda x: re.sub(r'\s\s+', ' ', str(x)))
+
+def tokenize(df, col):
+    """
+        Function to tokenize column of strings without punctuation
+        Input into word_tokenize() must be string with spaces only
+        Output is a list of tokenized words
+    """
+    text = ' '.join(df[col].to_list())
+    tokens = nltk.word_tokenize(text)
+    return tokens
+
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english'))
+def no_stopwords(text):
+    lst = [word for word in text if word not in stop_words]
+    return lst
+
+def term_frequency(df):
+    tf1 = (df['tweet'].apply(lambda x: pd.value_counts(x.split(" "))).sum(axis=0).reset_index())
+    tf1.columns = ['words', 'tf']
+    tf1 = tf1.sort_values(by='tf', ascending=False).reset_index()
+    return tf1
+
+def stemming(token_list):
+    ss = PorterStemmer()
+    lst = [ss.stem(w) for w in token_list]
+    return lst
+
+def lemmatization(df):
+    df['lem'] = df['tweet'].apply(lambda x: " ".join([Word(word).lemmatize() for word in x.split()]))
+    return df['lem'].head()
+
